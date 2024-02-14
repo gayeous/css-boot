9967290f99b9084f5ef36ad3512763f42589f596
ogaye <gayeous@gmail.com>
2024-02-14 12:40:58 +0000

    adding repository

---
+++ src/main/java/com/css/repository/TutorialRepository.java
@@ -0,0 +1,14 @@
+package com.css.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.css.cssboot.model.Tutorial;
+
+ 
+
+public interface TutorialRepository extends JpaRepository<com.css.cssboot.model.Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
