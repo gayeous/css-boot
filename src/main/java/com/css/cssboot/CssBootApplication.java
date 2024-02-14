3f4dd6535ccf6038e834622b403cce9e2ca20f28
ogaye <gayeous@gmail.com>
2024-02-14 12:27:19 +0000

    Initial commit

---
+++ src/main/java/com/css/cssboot/CssBootApplication.java
@@ -0,0 +1,13 @@
+package com.css.cssboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssBootApplication.class, args);
+	}
+
+}
